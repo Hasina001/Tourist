f02981ddad8999f98804ef54fa4efa1eb6eca5a1
Hasina <nyhasinafanonerantsoa@gmail.com>
2025-04-02 11:54:36 +0300

    Création de réacr-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
+import { Outlet } from "react-router-dom"
 
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline text-red-400">
-    Hello world!
-  </h1>
+      <div className="bg-[#F7F8F9] min-h-screen flex flex-col">
+        <nav>Navbar</nav>
+        <div className="flex-grow">
+          <Outlet/>
+        </div>
+        <footer className="mt-auto">Footer</footer>
+      </div>
     </>
   )
 }
 
 export default App
+ 
